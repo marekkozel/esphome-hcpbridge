49a701056922b3fc45b9cf6c3cea6ea61468b28e
Marek Kozel <75679236+marekkozel@users.noreply.github.com>
2025-01-04 16:04:40 +0100

    Update hoermann.h

--- components/hcpbridge/hoermann.h
+++ components/hcpbridge/hoermann.h
@@ -4,33 +4,34 @@
 #define HOERMANN_H_
 #define MODBUSRTU_DEBUG 1
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/uart.h"
-#include "esp_log.h"
-#include "driver/gpio.h"
+#include <Arduino.h>
+#include <Stream.h>
+
 #include "ModbusRTU.h"
 
 #define SLAVE_ID 2
 #define SIMULATEKEYPRESSDELAYMS 100
 #define DEADREPORTTIMEOUT 60000
 
-#define UART_PORT UART_NUM_2
+#define RS485 Serial2
 #ifdef CONFIG_IDF_TARGET_ESP32S3
 #define PIN_TXD 17
 #define PIN_RXD 18
 #else
-#define PIN_TXD 17 // UART 2 TXD - GPIO17
-#define PIN_RXD 16 // UART 2 RXD - GPIO16
+#define PIN_TXD 17 // UART 2 TXT - G17
+#define PIN_RXD 16 // UART 2 RXD - G16 
 #endif
 
-// Workaround as my Supramatic did not Report the Status 0x0A when it's in vent Position
-// When the door is at position 0x08 and not moving, Status gets changed to Venting.
+// workaround as my Supramatic did not Report the Status 0x0A when it's en vent Position
+// When the door is at position 0x08 and not moving Status get changed to Ventig.
 #define VENT_POS 0x08
 
 static const char *TAG_HCI = "HCI-BUS";
 
-class HoermannCommand {
+
+
+class HoermannCommand
+{
 public:
     static const HoermannCommand STARTOPENDOOR;
     static const HoermannCommand STARTCLOSEDOOR;
@@ -40,6 +41,7 @@ public:
     static const HoermannCommand STARTTOGGLELAMP;
     static const HoermannCommand WAITING;
 
+public:
     uint16_t commandRegPlus2Value;
     uint16_t commandEndPlus2Value;
     uint16_t commandRegPlus3Value;
@@ -51,15 +53,19 @@ private:
         uint16_t commandEndPlus2Value,
         uint16_t commandRegPlus3Value,
         uint16_t commandEndPlus3Value)
-        : commandRegPlus2Value(commandRegPlus2Value),
-          commandEndPlus2Value(commandEndPlus2Value),
-          commandRegPlus3Value(commandRegPlus3Value),
-          commandEndPlus3Value(commandEndPlus3Value) {}
+    {
+        this->commandRegPlus2Value = commandRegPlus2Value;
+        this->commandEndPlus2Value = commandEndPlus2Value;
+        this->commandRegPlus3Value = commandRegPlus3Value;
+        this->commandEndPlus3Value = commandEndPlus3Value;
+    }
 };
 
-class HoermannState {
+class HoermannState
+{
 public:
-    enum State {
+    enum State
+    {
         OPEN,
         OPENING,
         CLOSED,
@@ -70,13 +76,13 @@ public:
         MOVE_HALF,
         STOPPED
     };
-
+    // All position values are from 0 to 100
     float targetPosition = 0;
     float currentPosition = 0;
     bool lightOn = false;
     State state = CLOSED;
-    std::string debugMessage = "initial";
-    unsigned long lastModbusResponse = 0;
+    String debugMessage = "initial";
+    unsigned long lastModbusRespone = 0;
     bool changed = false;
     bool debMessage = false;
     float gotoPosition = 0.0f;
@@ -85,47 +91,58 @@ public:
     void setTargetPosition(float targetPosition);
     void setGotoPosition(float setPosition);
     void setCurrentPosition(float currentPosition);
-    void setLightOn(bool lightOn);
+    void setLigthOn(bool lightOn);
     void recordModbusResponse();
     void clearChanged();
     void clearDebug();
     long responseAge();
     void setState(State state);
     void setValid(bool isValid);
+
 };
 
-class HoermannGarageEngine {
+class HoermannGarageEngine
+{
 public:
     HoermannState *state = new HoermannState();
 
-    static HoermannGarageEngine &getInstance();
+    static HoermannGarageEngine& getInstance();
 
     void setup(int8_t rx, int8_t tx);
     void handleModbus();
     Modbus::ResultCode onRequest(Modbus::FunctionCode fc, const Modbus::RequestData data);
     void setCommandValuesToRead();
-    uint16_t onDoorPositionChanged(TRegister *reg, uint16_t val);
+    uint16_t onDoorPositonChanged(TRegister *reg, uint16_t val);
     uint16_t onCurrentStateChanged(TRegister *reg, uint16_t val);
     uint16_t onLampState(TRegister *reg, uint16_t val);
 
+    /**
+     * Write on 0x9C41 , byte1: counter, byte2: command
+     */
     uint16_t onCounterWrite(TRegister *reg, uint16_t val);
+
+    /**
+     * Helper to set next Command and *not* skip Current Command before end was sent
+     */
     void setCommand(bool cond, const HoermannCommand *command);
 
+    /**
+     * Control Functions
+     */
     void stopDoor();
     void closeDoor();
     void openDoor();
-    void toggleDoor();
+    void toogleDoor();
     void halfPositionDoor();
     void ventilationPositionDoor();
     void turnLight(bool on);
-    void toggleLight();
+    void toogleLight();
     void setPosition(int setPosition);
 
 private:
     HoermannGarageEngine(){};
-    ModbusRTU mb;                                 
-    const HoermannCommand *nextCommand = nullptr; 
-    unsigned long commandWrittenOn = 0;           
+    ModbusRTU mb;                                 // ModbusRTU instance, the man behind the curtain
+    const HoermannCommand *nextCommand = nullptr; // Next Command to transmit
+    unsigned long commandWrittenOn = 0;           // When was last command written (wait 100ms before end of command is transmitted)
 };
-
 #endif
